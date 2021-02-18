10260cd481774398785e97f049595e0dfc8e05d5
tani <tani@1-10.com>
2021-02-18 10:01:25 +0900

    Update 環境構築(ts, eslint, prettier)

---
+++ duck/myButton/countReaducer.ts
@@ -0,0 +1,6 @@
+const initialState = {
+  counter: 0,
+}
+
+const aa = 111
+const str = 'str' + 'a'
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return <div className={styles.container}>test</div>
+}
