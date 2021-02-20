6ce6302a22857e8fe3c471dd40f4a2bec6aab526
tani <tani@1-10.com>
2021-02-20 09:08:34 +0900

    update store app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import React, { FC } from 'react'
 import '../styles/globals.css'
+import React, { FC } from 'react'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
 
-const MyApp: FC = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+import { Provider } from 'react-redux'
+import { store } from '@/store'
+
+const MyApp: FC = ({ Component, pageProps }: AppProps) => {
+  return (
+    <React.Fragment>
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </React.Fragment>
+  )
 }
 
 export default MyApp
